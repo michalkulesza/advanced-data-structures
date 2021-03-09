283089af0300e2050d6b1b41719eb9fa406759cc
Michal <kulesza.michal@gmail.com>
2021-03-09 12:55:09 +0100

    + unshift

--- 2. Doubly Linked Lists.js
+++ 2. Doubly Linked Lists.js
@@ -64,6 +64,22 @@ class DoublyLinkedList {
 		this.length--;
 		return oldHead;
 	}
+
+	unshift(val) {
+		const newNode = new Node(val);
+
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+
+		this.length++;
+		return this;
+	}
 }
 
 const list = new DoublyLinkedList();
